004d5ce68f97d662b867ac7593d55921482a8b77
cfalcon <cfalcon@cells.es>
2018-05-09 12:56:04 +0200

    Add opus cmd to control Tango Stage motors
    Add opus cmd interface to communicate with the
    serial line.

--- opusserver/opusserver.py
+++ opusserver/opusserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import time
+import serial
 import socket
 import win32pipe
 import win32file
@@ -9,15 +10,19 @@ from threading import (Lock, Thread)
 
 
 class SocketListenerThread(Thread):
-
     BUFFER_SIZE = 4096
+    SLEEPING_SERIAL_TIME = 0.05
 
     def __init__(self, serversock):
         Thread.__init__(self)
+        # Server Socket
         self.serversock = serversock
         self.serve = True
         self.clientsock = None
         self.createFileHandle()
+        # Serial connection
+        self._ser = None
+        # Last server command
         self._last_cmd = None
 
     def runOpus(self):
@@ -84,6 +89,16 @@ class SocketListenerThread(Thread):
         if cmd.lower() == "exit":
             self.shutdown()
 
+        # Serial commands
+        if cmd.startswith('send_serial_cmd'):
+            _, serial_cmd = cmd.split(' ', 1)
+            if self._ser is None:
+                self._create_serial_connection()
+
+            serial_output = self.send_serial_cmd(serial_cmd)
+            return "{0}\n".format(serial_output)
+
+        # Pipe commands
         if cmd.lower() == "s_pipe":
             if self.isPIPEConnected:
                 return "{0}\n".format('Pipe is connected')
@@ -97,12 +112,13 @@ class SocketListenerThread(Thread):
             else:
                 return "{0}\n".format('Pipe is not connected')
 
-#        if cmd.startwith('rm '):
-#            path = cmd.split('rm ')
-#            files = glob.glob('{0}/*'.format(path))
-#            for f in files:
-#                os.remove(f)
-#            return "ok\n"
+                # Opus commands
+                #        if cmd.startwith('rm '):
+                #            path = cmd.split('rm ')
+                #            files = glob.glob('{0}/*'.format(path))
+                #            for f in files:
+                #                os.remove(f)
+                #            return "ok\n"
 
         cmd = "{0}\n".format(cmd)
         print("writing... {0}".format(cmd))
@@ -128,12 +144,14 @@ class SocketListenerThread(Thread):
     def _run_macro_with_args(self, cmd):
         # Run Opus macros with arguments
         splitted_cmd = cmd.split()
-        nargs = (len(splitted_cmd)-2)/2
-        run_cmd = "{0} {1} {2}\n".format(splitted_cmd[0], splitted_cmd[1], nargs)
+        nargs = (len(splitted_cmd) - 2) / 2
+        run_cmd = "{0} {1} {2}\n".format(splitted_cmd[0], splitted_cmd[1],
+                                         nargs)
         win32file.WriteFile(self.fileHandle, run_cmd)
         for i in range(nargs):
-            run_cmd = "WRITE_PARAMETER {0} {1}\n".format(splitted_cmd[2 + i * 2].lower(),
-                                                         splitted_cmd[3 + i * 2])
+            run_cmd = "WRITE_PARAMETER {0} {1}\n".format(
+                splitted_cmd[2 + i * 2].lower(),
+                splitted_cmd[3 + i * 2])
             win32file.WriteFile(self.fileHandle, run_cmd)
         data = self.readFileHandle()
         return "{0}\n".format(data)
@@ -158,8 +176,6 @@ class SocketListenerThread(Thread):
             value = ans.split('\n')[1]
         else:
             value = "NaN"
-        # Unload selected file
-        self._unload_selected()
 
         return "{0}\n".format(value)
 
@@ -181,8 +197,40 @@ class SocketListenerThread(Thread):
         self.serversock.shutdown(0)()
         self.fileHandle.close()
 
+    def _create_serial_connection(self, port='COM4', baudrate=57600,
+                                  timeout=1):
+        self._ser = serial.Serial(port=port, baudrate=baudrate,
+                                  timeout=timeout)
+        self._ser.isOpen()
 
-if __name__ == '__main__':
+    def send_serial_cmd(self, cmd):
+        if cmd == 'exit':
+            self._ser.close()
+            self._ser = None
+            return "OK"
+        else:
+            if self._ser.isOpen():
+                # flush input buffer, discarding all its contents
+                self._ser.flushInput()
+                # flush output buffer, aborting current output and discard all
+                # that is in buffer
+                self._ser.flushOutput()
+
+            # send the character to the device
+            # \r\n carriage return and line feed to the characters
+            # this is requested by my device
+            eol = '\r\n'
+            self._ser.write(cmd + eol)
+            out = ''
+            # let's wait before reading output
+            # (let's give device time to answer)
+            time.sleep(self.SLEEPING_SERIAL_TIME)
+            while self._ser.inWaiting() > 0:
+                out += self._ser.read(1)
+            return out
+
+
+def run_opus_server():
     lock = Lock()
     ip = socket.gethostbyname(socket.gethostname())
     ADDR = (ip, 5000)
@@ -194,6 +242,8 @@ if __name__ == '__main__':
     slt = SocketListenerThread(serversock)
     slt.start()
 
+if __name__ == '__main__':
+    run_opus_server()
 
     """ TEST IT
     import socket, time
@@ -206,25 +256,27 @@ if __name__ == '__main__':
         sock.sendall(cmd+'\n')
         ans = sock.recv(4096)
         print cmd,'->',ans
-    
+
+    run_cmd("send_serial_cmd ?pos")
+
     run_cmd("GET_VERSION")
     run_cmd("MEASURE_SAMPLE <Experiment file>")
-    e.g  
+    e.g
     run_cmd("MEASURE_SAMPLE L:\controls\devel\cfalcon\MCT_TRANS.XPM")
     run_cmd("READ_FROM_BLOCK")
-    
+
     run_cmd("READ_FROM_FILE <file>")
     e.g.  run_cmd("READ_FROM_FILE L:\controls\devel\cfalcon\spectrum.1")
     "READ_FROM_FILE C:\users\sicilia\opus_test\spectrum.1"
     run_cmd("START_MACRO <Macro file name>[<Number of input parameters>]") # wait
     run_cmd("KILL_MACRO <MacroID>")
-    run_cmd("RUN_MACRO <Macro file name>[<Number of input parameters>]") 
+    run_cmd("RUN_MACRO <Macro file name>[<Number of input parameters>]")
 
     e.g.
     run_cmd("RUN_MACRO L:\controls\devel\cfalcon\MeasureSample.mtx 1")
     run_cmd("WRITE_PARAMETER lct 10")
 
- 
+
     run_cmd("START_MACRO C:\Users\Public\Documents\Bruker\OPUS_7.5.18\at\macro\Alignment\MeasureSample.mtx 1")
 
     run_cmd("MEASURE_SAMPLE L:\controls\devel\cfalcon\MCT_TRANS.XPM")
