9557c3c7e43836f933902151439e8eced021a456
cfalcon <cfalcon@cells.es>
2018-05-24 16:32:36 +0200

    Bump version 0.1.0 to 0.2.0-alpha

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '0.1.0'
+__version = '0.2.0-alpha'
 
 long_description = """This is a DS to remotely manipulate the OPUS
 Spectroscopy Software (BRUKER) from Linux to Windows.
